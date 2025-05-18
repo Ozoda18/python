b5a6ffd9e8a995993704bfae5b695cf835c4ad8e
Ozoda18 <ozodaeshnazarova9@gmail.com>
2025-05-18 19:46:24 +0500

    Add files via upload

--- lesson-7/homework/ls7.hw.py
+++ lesson-7/homework/ls7.hw.py
@@ -1,1 +1,22 @@
+def is_prime(n): 
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False 
+        else:
+            return True
+print(is_prime(4));
+print(is_prime(7));
+
+def digit_sum(k):
+    return sum(int(digit) for digit in str(k))
+print(digit_sum(24));
+print(digit_sum(502));
+
+def power(m):
+    n=1
+    while 2**n<=m:
+        print(2**n,end=' ')
+        n+=1
+print(power(16));     
+
 
