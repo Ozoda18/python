c8634fc00b7cfc62b22f4907a0ae790176567009
Ozoda18 <ozodaeshnazarova9@gmail.com>
2025-05-30 21:51:43 +0500

    Add files via upload

---
+++ lesson-10/homework/ls10.py
@@ -0,0 +1,13 @@
+class Ball:
+    def  __init__(self,color,size,weight):
+        self.color=color
+        self.size=size
+        self.weight=weight
+ball1=Ball('red',13,.5)
+ball2=Ball('green',14,.7)  
+print(ball1.color)   ;   
+
+class Ball:
+    pass
+ball_obj=Ball()
+ball_obj.size=12
